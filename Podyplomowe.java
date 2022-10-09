00f6d4f3dae49d78e2ea35ae1719a50cbc14a692
mkulak <mkulak@student.wszib.edu.pl>
2022-10-09 14:41:43 +0200

    W tym pliku zostało dodane zdjęcie.

--- Podyplomowe.java
+++ Podyplomowe.java
@@ -1,5 +1,6 @@
 public class Podyplomowe {
     public static void main(String[] args) {
         System.out.println("Studia podyplomowe!");
+        //Ten program wyświetla napis "Studia podyplomowe"
     }
 }
